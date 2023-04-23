74f3599858ac687700d131bae035186b3e502c41
Darius Karneckij <dkarneckij@gmail.com>
2023-04-23 17:17:58 -0400

    Header and basic container

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = document.title;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title} />
+      <div className="wrapper">
+        Test
+      </div>
     </div>
   );
+}
+
+function Header(props) {
+  return <h1 className="App-header"
+    >{props.title}
+    </h1>;
 }
 
 export default App;
